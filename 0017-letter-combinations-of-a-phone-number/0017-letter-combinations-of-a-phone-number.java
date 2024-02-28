2165dd5812be83c5cce19dd88485adf812990751
Parth Pidadi <94888172+Parth-Pidadi@users.noreply.github.com>
2024-02-28 16:04:56 +0530

    Time: 6 ms (14.50%), Space: 42.3 MB (33.41%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,24 +1,35 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        ArrayList<String> ans = new ArrayList<>();
-        if (digits.length() == 0) // Base case: If input is empty, return empty list
-            return ans;
+        // ArrayList<String> ans = new ArrayList<>();
+        String ans = "";
         
-        String[] opt = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        phonePad(digits, ans, opt, 0, "");
-        return ans;
+        ArrayList<String> opt = new ArrayList<>();
+        Collections.addAll(opt, ""," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" );
+        if(digits.length() == 0){
+            return new ArrayList<>() ;
+        }
+      
+        
+        
+        
+        return phonePad(digits, ans, opt);
+        
+    }
+    ArrayList<String> phonePad(String digits, String ans, ArrayList<String> opt){
+        if(digits.length() == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(ans);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        int ch = digits.charAt(0) - '0';
+        String alpha = opt.get(ch);
+        for(int i=0; i<alpha.length();i++){
+            char dh = alpha.charAt(i);
+            list.addAll(phonePad(digits.substring(1), ans + dh, opt));
+            
+        }
+        return list;
     }
     
-    void phonePad(String digits, List<String> ans, String[] opt, int index, String current) {
-        if (index == digits.length()) {
-            ans.add(current);
-            return;
-        }
-        
-        int digit = digits.charAt(index) - '0';
-        String letters = opt[digit];
-        for (int i = 0; i < letters.length(); i++) {
-            phonePad(digits, ans, opt, index + 1, current + letters.charAt(i));
-        }
-    }
 }
