aa96966fb983434e2957b756e478352562fa6781
Parth Pidadi <94888172+Parth-Pidadi@users.noreply.github.com>
2024-03-12 15:04:14 +0530

    Time: 1 ms (44.94%), Space: 40.5 MB (80.31%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        // if ((Math.log(n))/(Math.log(2)) % 1 == 0.0 ){
+        //     return true;
+        // } else{
+        //     return false;
+        // }
+        // System.out.println((Math.log(n))/(Math.log(2)));
+        if(n<1){
+            return false;
+        }
+        if (n==1){
+            return true;
+        }
+        if(n%2 ==1){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+           
+    }
+}
