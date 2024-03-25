aa69fbbdf8609feb959003a35226626071d63393
Parth Pidadi <94888172+Parth-Pidadi@users.noreply.github.com>
2024-03-25 15:21:37 +0530

    Time: 0 ms (100.00%), Space: 44.5 MB (47.97%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -27,55 +27,15 @@ public class Solution {
                   fast = fast.next;
                   slow = slow.next;
               }
-            //     length = cycleLength(slow);
-            //     break;
-            // }
+           
              return slow;
            }
         
         }
-        // if(length == 0) return null;
-        // //find the start node;
-        // ListNode f = head;
-        // ListNode s = head;
-        // while(length > 0)
-        // {
-        //     s = s.next;
-        //     length--;
-        // }
-
-        // //keep moving both forward and they will meet at cycle start
-        // while(f != s)
-        // {
-        //     s = s.next;
-        //     f = f.next;
-        // }
+      
         return null;
         
     }
    
-    // public int cycleLength(ListNode head)
-    // {    
-    //     ListNode slow = head;
-    //     ListNode fast = head;
-    //     while(fast != null && fast.next != null)
-    //     {
-    //         fast = fast.next.next;
-    //         slow = slow.next;
-    //         if(fast == slow)
-    //         {
-    //             //calculate the length of cycle
-    //             ListNode temp = slow;
-    //               int length = 0;
-    //               do{
-    //                   temp = temp.next;
-    //                   length++;
-    //               }
-    //               while(slow != temp);
-    //               return length;
-
-    //         }
-    //     }
-    //     return 0;
-    // }
+   
 }
