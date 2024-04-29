c603268b3e65e6a03185dfe919444c7b36088bfe
Parth Pidadi <94888172+Parth-Pidadi@users.noreply.github.com>
2024-04-29 14:42:55 +0530

    Time: 50 ms (42.49%), Space: 19.2 MB (81.76%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,10 +5,11 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.diameter = 0
+    
+        
 
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.diameter = 0
         
         def height(node):
             if not node:
